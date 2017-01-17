e6a20c27c1a05999d3b8c8985e99e18c9c873d4a
Sarah Daugherty <sarah@balarah.net>
2017-01-17 06:53:20 -0500

    Assignment 11 Album View: correction

--- scripts/album.js
+++ scripts/album.js
@@ -71,12 +71,13 @@ var createSongRow = function(songNumber, songName, songLength) {
   return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
@@ -88,18 +89,17 @@ var setCurrentAlbum = function(album) {
     albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
   }
 };
- 
-albums = [albumPicasso, albumMarconi, albumBestOfClassicBananasInPyjamas];
-var albumListIndex = 0;
 
 window.onload = function() {
-  setCurrentAlbum(albums[albumListIndex]);
-};
+  setCurrentAlbum(albumPicasso);
 
-document.getElementsByClassName('album-cover-art')[0].addEventListener('click', function() {
-  albumListIndex++;
-  if (albumListIndex === albums.length) {
-    albumListIndex = 0;
-  };
-  setCurrentAlbum(albums[albumListIndex]);
-});
+  var albums = [albumPicasso, albumMarconi, albumBestOfClassicBananasInPyjamas];
+  var albumListIndex = 1;
+  albumImage.addEventListener('click', function(event) {
+    setCurrentAlbum(albums[albumListIndex]);
+    albumListIndex++;
+    if (albumListIndex === albums.length) {
+        albumListIndex = 0;
+    }
+  });
+};
