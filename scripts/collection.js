fab89b9a9597a565cca1a499a3373030f34ea5a6
Sarah Daugherty <sarah@balarah.net>
2017-01-18 06:30:14 -0500

    Assignment 11 Album View: correction
    fixed collection view

--- scripts/collection.js
+++ scripts/collection.js
@@ -13,3 +13,12 @@ var collectionItemTemplate =
   + '  </div>'
   + '</div>'
   ;
+
+window.onload = function() {
+  var collectionContainer = document.getElementsByClassName('album-covers')[0];
+  collectionContainer.innerHTML = '';
+  for (var i = 0; i < 12; i++) {
+    collectionContainer.innerHTML += collectionItemTemplate;
+  }
+}
+  
