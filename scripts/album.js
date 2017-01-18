e7575f41834e206defd4bc026abd809af5e2683b
Sarah Daugherty <sarah@balarah.net>
2017-01-18 06:56:15 -0500

    Checkpoint 11 Album View: correction 2
    For some reason the track list went missing when I did the last correction. I think I made a mess haha.

--- scripts/album.js
+++ scripts/album.js
@@ -29,14 +29,14 @@ var albumMarconi = {
 };
 
 var createSongRow = function(songNumber, songName, songLength) {
-  var template = 
-      '<tr class="album-view-song-item">'
+  var template =
+    '<tr class="album-view-song-item">'
   + '  <td class="song-item-number">' + songNumber + '</td>'
   + '  <td class="song-item-title">' + songName + '</td>'
   + '  <td class="song-item-duration">' + songLength + '</td>'
   + '</tr>'
   ;
-  
+
   return template;
 };
 
@@ -46,19 +46,19 @@ var setCurrentAlbum = function(album) {
   var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
   var albumImage = document.getElementsByClassName('album-cover-art')[0];
   var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-  
+ 
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
   albumImage.setAttribute('src', album.albumArtUrl);
-  
+
   albumSongList.innerHTML = '';
-  
+ 
   for (var i = 0; i < album.songs.length; i++) {
     albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
   }
 };
-
-window.onload = funtion () {
+ 
+window.onload = function() {
   setCurrentAlbum(albumPicasso);
 };
